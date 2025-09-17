d8b065aad060b517eaaa79a301f35827351d8de2
Максим Суббота <maksimsubbota@MacBook-Air-Maksim.local>
2025-09-17 11:24:44 +0800

    Интеграция с Firebase для хранения покупок

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/', 
-  build: {
-    outDir: 'docs'
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    // В зависимости от режима, выбираем нужный base
+    base: mode === 'gh-pages' ? '/tene/' : '/',
+    build: {
+      outDir: 'docs'
+    }
   }
 })

9317c06e304562b46aad9a0f40b470d21fb8dc88
Alena Subbota <alenasubbota@bk.ru>
2025-10-14 15:36:00 +0800

    first commit

--- src/components/NewsModal.jsx
+++ src/components/NewsModal.jsx
@@ -1,11 +1,11 @@
 export const NewsModal = ({ newsItem, onClose }) => (
     <div className="fixed inset-0 bg-black/50 backdrop-blur-sm z-50 flex items-center justify-center p-4" onClick={onClose}>
         <div className="w-full max-w-md p-6 rounded-2xl shadow-lg bg-component-bg text-text-main flex flex-col" onClick={e => e.stopPropagation()}>
-            <h2 className="text-2xl font-bold mb-4">{newsItem.title}</h2>
+            <h2 className="text-2xl font-bold mb-4 break-words">{newsItem.title}</h2>
             
             {/* 👇 ИСПРАВЛЕНИЕ ЗДЕСЬ 👇 */}
             <div 
-              className="prose prose-sm max-w-none text-text-main opacity-80 overflow-y-auto max-h-[60vh]"
+              className="prose prose-sm max-w-none text-text-main opacity-80 overflow-y-auto max-h-[60vh] "
               dangerouslySetInnerHTML={{ __html: newsItem.fullText }} 
             />
             

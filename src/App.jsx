3f7f11a73a69551b0d3305ae67d17752f3bcbd14
Alena Subbota <alenasubbota@bk.ru>
2025-10-28 14:09:24 +0800

    Trigger deploy with updated secrets

--- src/App.jsx
+++ src/App.jsx
@@ -360,7 +360,7 @@ const handleFontChange = (newFontClass) => {
 
   const renderContent = () => {
     if (page === 'details') {
-      return <NovelDetails novel={selectedNovel} onSelectChapter={handleSelectChapter} onGenreSelect={handleSelectGenre} subscription={subscription} botUsername={BOT_USERNAME} userId={userId} chapters={chapters} isLoadingChapters={isLoadingChapters} lastReadData={lastReadData} onBack={handleBack} bookmarks={bookmarks} onToggleBookmark={handleToggleBookmark}/>;
+      return <NovelDetails novel={selectedNovel} onSelectChapter={handleSelectChapter} onGenreSelect={handleGenreSelect} subscription={subscription} botUsername={BOT_USERNAME} userId={userId} chapters={chapters} isLoadingChapters={isLoadingChapters} lastReadData={lastReadData} onBack={handleBack} bookmarks={bookmarks} onToggleBookmark={handleToggleBookmark}/>;
     }
     if (page === 'reader') {
     const displayName = user?.user_metadata?.full_name || 

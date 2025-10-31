e2a96c57d1b203a5ecb3149054eae26fc8623d38
Alena Subbota <alenasubbota@bk.ru>
2025-10-31 09:41:00 +0800

    Trigger deploy with updated secrets

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { AuthProvider } from './Auth.jsx'; // <-- 1. Импортируйте AuthProvider
+import { AuthProvider } from './Auth.jsx';
+import { BrowserRouter } from 'react-router-dom'; // <-- 1. ИМПОРТ
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <-- 2. Оберните App в AuthProvider --> */}
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <BrowserRouter> {/* <-- 2. Обертка #1 */}
+      <AuthProvider> {/* <-- 3. Обертка #2 */}
+        <App />
+      </AuthProvider>
+    </BrowserRouter> {/* <-- 4. Закрыть обертку #1 */}
   </React.StrictMode>,
 )
